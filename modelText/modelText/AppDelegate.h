e008cb0a1ac67be55253b506ef27c841d4ac0c10
yeyuranshan <609004893@qq.com>
2017-11-06 15:53:09 +0800

    提交

---
+++ modelText/modelText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  modelText
+//
+//  Created by 潘振权 on 2017/11/6.
+//  Copyright © 2017年 潘振权. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
